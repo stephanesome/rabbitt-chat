da327b5042e5fdd3e5b884570a741e44bf15e983
stephane <stephanesome@gmail.com>
2023-07-11 16:59:57 -0400

    Initial commit

---
+++ src/main/kotlin/seg3x02/rabbittchat/RabbittChatApplication.kt
@@ -0,0 +1,11 @@
+package seg3x02.rabbittchat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbittChatApplication
+
+fun main(args: Array<String>) {
+	runApplication<RabbittChatApplication>(*args)
+}
---
+++ src/test/kotlin/seg3x02/rabbittchat/RabbittChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package seg3x02.rabbittchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabbittChatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
